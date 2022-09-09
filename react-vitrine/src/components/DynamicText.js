c387c86c1db3b421630f4749dbd49196c8ef228c
Thomas Vieillecourt <thomas.viei@hotmail.fr>
2022-09-09 16:38:37 +0200

    Anim text

---
+++ react-vitrine/src/components/DynamicText.js
@@ -0,0 +1,97 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const DynamicText = () => {
+  const reactArray = "React".split("");
+
+  const animText = keyframes`
+  0%{ opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(-30deg); filter:blur(6px);}
+  25%{ opacity: 1; transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter:blur(0px);}
+  75%{ opacity: 1; transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter:blur(0px);}
+  100% { opacity: 0; transform: translateY(80px); filter:blur(2px);}
+  `;
+
+  const Wrapper = styled.h2`
+    display: inline-block;
+
+    span {
+      display: inline-block;
+      opacity: 0;
+      animation-name: ${animText};
+      animation-duration: 6s;
+      aniamtion-fill-mode: forwards;
+      animation-iteration-count: infinite;
+      animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
+
+    span:nth-child(1) {
+      animation-delay: 0.1s;
+    }
+
+    span:nth-child(2) {
+      animation-delay: 0.2s;
+    }
+
+    span:nth-child(3) {
+      animation-delay: 0.3s;
+    }
+
+    span:nth-child(4) {
+      animation-delay: 0.4s;
+    }
+
+    span:nth-child(5) {
+      animation-delay: 0.5s;
+    }
+  `;
+
+  //   useEffect(() => {
+  //     const target = document.getElementById("text-target");
+  //     let textArray = ["Thomas", "clear", "smart", "strong"];
+  //     let wordIndex = 0;
+  //     let letterIndex = 0;
+
+  //     const createLetter = () => {
+  //       const letter = document.createElement("span");
+
+  //       target.appendChild(letter);
+  //       letter.classList.add("letter");
+  //       letter.style.opacity = "0";
+  //       letter.style.animation = "anim 5s ease forwards";
+  //       letter.textContent = textArray[wordIndex][letterIndex];
+
+  //       setTimeout(() => {
+  //         letter.remove();
+  //       }, 2000);
+  //     };
+
+  //     const loop = () => {
+  //       setTimeout(() => {
+  //         if (wordIndex >= textArray.length) {
+  //           wordIndex = 0;
+  //           letterIndex = 0;
+  //           loop();
+  //         } else if (letterIndex < textArray[wordIndex].length) {
+  //           createLetter();
+  //           letterIndex++;
+  //           loop();
+  //         } else {
+  //           letterIndex = 0;
+  //           wordIndex++;
+  //           setTimeout(loop, 2000);
+  //         }
+  //       }, 80);
+  //     };
+  //     loop();
+  //   }, []);
+
+  return (
+    <Wrapper>
+      {reactArray.map((item, index) => (
+        <span key={index}>{item}</span>
+      ))}
+    </Wrapper>
+  );
+};
+
+export default DynamicText;
--- react-vitrine/src/pages/Home.js
+++ react-vitrine/src/pages/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Buttons from "../components/Buttons";
+import DynamicText from "../components/DynamicText";
 import Navigation from "../components/Navigation";
 import SocialNetwork from "../components/SocialNetwork";
 
 const Home = () => {
   return (
     <div>
-      <Navigation />
-      <SocialNetwork />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>Thomas</h1>
+            <DynamicText />
+          </div>
+        </div>
+
+        <Buttons right={"/projet-1"} />
+      </div>
     </div>
   );
 };
